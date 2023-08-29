cc170c018ad62760b5c81072e516c3f99745353d
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-08-29 21:48:15 +0545

    Add proxy || add build & deploy scripts

---
+++ README.md
@@ -0,0 +1,1 @@
+https://phonebook-n0jr.onrender.com
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.static("dist"));
+
 app.use(express.json());
 app.use(cors());
 
